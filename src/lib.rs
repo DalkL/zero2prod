7fcaacf99af4abb5ea30b98199f5177a9b8c840b
DalkL <dalkiriadis.lukas@gmail.com>
2024-03-11 13:04:09 -0400

    Added subscription early implementation and tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,20 @@
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer, HttpResponse};
 use actix_web::dev::Server;
 use std::net::TcpListener;
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
+}
+
+
 async fn health_check() -> HttpResponse {
+    return HttpResponse::Ok().finish();
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     return HttpResponse::Ok().finish();
 }
 
@@ -10,6 +22,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new( || {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
