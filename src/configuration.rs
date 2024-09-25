b5ae4f760952879b236ff5c2081e0c93d40e68e7
DalkL <dalkiriadis.lukas@gmail.com>
2024-09-24 22:53:13 -0400

    Configuration bugfix

--- src/configuration.rs
+++ src/configuration.rs
@@ -88,8 +88,8 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
 
 impl DatabaseSettings {
     pub fn with_db(&self) -> PgConnectOptions {
-        let mut options = self.without_db().database(&self.database_name);
-        options.log_statements(tracing_log::log::LevelFilter::Trace);
+        let options = self.without_db().database(&self.database_name);
+        options.clone().log_statements(tracing_log::log::LevelFilter::Trace);
         return options;
     }
 
