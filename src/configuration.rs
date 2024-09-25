47b6fe981a3c97108eb2ccfa773938334fef56c3
DalkL <dalkiriadis.lukas@gmail.com>
2024-09-24 22:22:40 -0400

    Changes for deployment p157

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,4 +1,8 @@
 use secrecy::{Secret, ExposeSecret};
+use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::PgSslMode;
+use sqlx::ConnectOptions;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -8,17 +12,20 @@ pub struct Settings {
 
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String
 }
 
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub port: u16,
     pub username: String,
     pub password: Secret<String>,
-    pub port: u16,
     pub host: String,
-    pub database_name: String
+    pub database_name: String,
+    pub require_ssl: bool
 }
 
 pub enum Environment {
@@ -69,25 +76,36 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         .add_source(
             config::File::from(configuration_directory.join(environment_filename))
         )
+        .add_source(
+            config::Environment::with_prefix("APP")
+            .prefix_separator("_")
+            .separator("_")
+        )
         .build()?;
 
     return settings.try_deserialize::<Settings>();
 }
 
 impl DatabaseSettings {
-    pub fn connection_string(&self) -> Secret<String> {
-        return Secret::new(format!(
-            "postgres://{}:{}@{}:{}/{}",
-            self.username, self.password.expose_secret(), self.host, self.port, self.database_name
-        ));
+    pub fn with_db(&self) -> PgConnectOptions {
+        let mut options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing_log::log::LevelFilter::Trace);
+        return options;
     }
 
-    pub fn connection_string_without_db(&self) -> Secret<String> {
-        return Secret::new(format!(
-            "postgres://{}:{}@{}:{}",
-            self.username, self.password.expose_secret(), self.host, self.port
-        ));
+    pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
 
+        return PgConnectOptions::new()
+            .host(&self.host)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .port(self.port)
+            .ssl_mode(ssl_mode);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use std::net::TcpListener;
-use sqlx::postgres::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -11,10 +10,8 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect_lazy(
-            &configuration.database.connection_string().expose_secret()
-        )
-        .expect("Failed to connect to database");
+    let connection_pool = PgPoolOptions::new()
+        .connect_lazy_with(configuration.database.with_db());
     let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
 
